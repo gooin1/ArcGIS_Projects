0f9938deed4e60ebb6f360de754e80d03e106176
gooin <gooin@outlook.com>
2017-12-04 15:40:48 +0800

    dojo declare 的单继承

--- dojo_tutorial/DeclareClass.js
+++ dojo_tutorial/DeclareClass.js
@@ -1,4 +1,5 @@
-require(["dojo/_base/declare", "esri/Map"], function (declare, Map) {
+require(["dojo/_base/declare"], function (declare) {
+
     declare("myApp.examples.Shape", // 类名
         null, // 无父类，使用null
         {
@@ -8,81 +9,43 @@ require(["dojo/_base/declare", "esri/Map"], function (declare, Map) {
             }
         }
     );
-    //
-    // declare("myApp.examples.Circle",
-    //     myApp.examples.Shape,
-    //     {
-    //         radius: 0,
-    //
-    //         constructor: function (radius) {
-    //             this.radius = radius || this.radius;
-    //         },
-    //
-    //         setRadius: function (radius) {
-    //             this.radius = radius;
-    //         },
-    //
-    //         area: function () {
-    //             return Math.PI * this.radius * this.radius;
-    //         }
-    //     }
-    // );
-    //
-    // declare("myApp.examples.Rectangle",
-    //     myApp.examples.Shape,
-    //     {
-    //         length: 0,
-    //         width: 0,
-    //
-    //         constructor: function (l, w) {
-    //             this.length = l || this.length;
-    //             this.width = w || this.width;
-    //         },
-    //
-    //         setLength: function (l) {
-    //             this.length = l;
-    //         },
-    //
-    //         setWidth: function (w) {
-    //             this.width = w;
-    //         },
-    //
-    //         area: function () {
-    //             return this.length * this.width;
-    //         }
-    //     }
-    // );
-    //
-    // declare("myApp.examples.Position",
-    //     null,
-    //     {
-    //         x: 0,
-    //         y: 0,
-    //
-    //         constructor: function (x, y) {
-    //             this.x = x || this.x;
-    //             this.y = y || this.y;
-    //         },
-    //
-    //         setPosition: function (x, y) {
-    //             this.x = x;
-    //             this.y = y;
-    //         },
-    //
-    //         move: function (deltaX, deltaY) {
-    //             this.x += deltaX;
-    //             this.y += deltaY;
-    //         }
-    //     }
-    // );
-    //
-    // declare("myApp.examples.PositionedCircle",
-    //     [myApp.examples.Circle, myApp.examples.Position],
-    //     {
-    //         constructor: function (radius, x, y) {
-    //             this.setPosition(x, y);
-    //         }
-    //     }
-    // );
+    // 圆
+    declare("myApp.examples.Circle",
+        myApp.examples.Shape, // 继承自Shape
+        {
+            radius: 0,
+            // 执行 new Circle 时的构造函数
+            constructor: function (radius) {
+                this.radius = radius || this.radius;
+            },
+            setRadius: function (radius) {
+                this.radius = radius;
+            },
+            area: function () {
+                return Math.PI * this.radius * this.radius;
+            }
+        }
+    );
+    // 矩形
+    declare("myApp.examples.Rectangle",
+        myApp.examples.Shape, // 继承自 Shape
+        {
+            length: 0,
+            width: 0,
+            constructor: function (length, width) {
+                this.length = length || this.length;
+                this.width = width || this.width;
+            },
+            setLength: function (l) {
+                this.length = l;
+            },
+            setWidth: function (w) {
+                this.width = w;
+            },
+            area: function () {
+                return this.length * this.width;
+            }
+        }
+    );
 
 });
